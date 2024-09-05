8af0a8426de52cc4ca2fd1859bb658aa1050d2dc
Toscanah <alego.cecchini@gmail.com>
2024-09-05 16:46:29 +0200

    Fixxed updating product category

--- app/(site)/sql/products/updateProduct.tsx
+++ app/(site)/sql/products/updateProduct.tsx
@@ -1,22 +1,30 @@
+import { isNull } from "util";
 import { ProductWithInfo } from "../../types/ProductWithInfo";
 import prisma from "../db";
 
 export default async function updateProduct(newProduct: ProductWithInfo) {
+  const existingProduct = await prisma.product.findFirst({
+    where: {
+      code: newProduct.code,
+      id: {
+        not: newProduct.id,
+      },
+    },
+  });
+
+  if (existingProduct) return null;
+
   return await prisma.product.update({
     where: {
       id: newProduct.id,
     },
     data: {
-      category: {
-        connect: {
-          id: Number(newProduct.category)
-        }
-      },
       code: newProduct.code,
       desc: newProduct.desc,
       site_price: newProduct.site_price,
       home_price: newProduct.home_price,
       rice: newProduct.rice,
+      category_id: Number(newProduct.category_id) !== -1 ? Number(newProduct.category_id) : null,
     },
     include: {
       category: {
