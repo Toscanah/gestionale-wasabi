c691fd2cd1420ec3bb27f02f169608d22394a48a
Toscanah <alego.cecchini@gmail.com>
2025-02-09 15:01:25 +0100

    Revert round to two function

--- app/(site)/functions/formatting-parsing/roundToTwo.ts
+++ app/(site)/functions/formatting-parsing/roundToTwo.ts
@@ -1,10 +1,3 @@
 export default function roundToTwo(amount: number | undefined): string {
-  if (amount === undefined) return (0).toFixed(2);
-
-  const scaled = amount * 100;
-  const rounded = Math.round(scaled); // Normal rounding
-  const decimalDiff = scaled - Math.floor(scaled); // Get the decimal part
-
-  // If the decimal part is exactly .50, always round up
-  return ((decimalDiff === 0.5 ? Math.ceil(scaled) : rounded) / 100).toString();
+  return (amount ?? 0).toFixed(2);
 }
