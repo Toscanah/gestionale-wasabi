8bcdabd6016a0465123d08b1eda018fbce9be412
Alessandro Cecchini <alego.cecchini@gmail.com>
2025-04-24 14:22:36 +0200

    Refactor TimeInput component for improved input handling and focus management.

--- app/(site)/components/ui/time/TimeInput.tsx
+++ app/(site)/components/ui/time/TimeInput.tsx
@@ -1,16 +1,31 @@
 import { Label } from "@/components/ui/label";
 import { TimeValue } from "./TimePicker";
 import { Input } from "@/components/ui/input";
+import useFocusOnClick from "@/app/(site)/hooks/useFocusOnClick";
 
 function displayValue(v: string, type: "h" | "m"): string {
-  if (v === "00") return "";
-  if (type === "m") return v;
-  return v.replace(/^0+(?=\d)/, "");
+  return v; // Pad single digit with leading zero
 }
 
 function sanitizeInput(raw: string): string {
-  return raw.replace(/\D/g, "").slice(0, 2);
+  return raw
+  // return raw.replace(/\D/g, "").slice(0, 2); // Keep only digits and limit to 2 characters
 }
+
+// function sanitizeInput(raw: string, type: "h" | "m"): string {
+//   const sanitized = raw.replace(/\D/g, "").slice(0, 2); // Keep only digits and limit to 2 characters
+//   const value = parseInt(sanitized, 10);
+
+//   if (isNaN(value)) return ""; // Return empty if the input is not a number
+
+//   if (type === "h") {
+//     return Math.min(Math.max(value, 0), 23).toString(); // Clamp hours between 0 and 23
+//   } else if (type === "m") {
+//     return Math.min(Math.max(value, 0), 59).toString(); // Clamp minutes between 0 and 59
+//   }
+
+//   return sanitized;
+// }
 
 interface TimeInputProps {
   label: string;
@@ -24,13 +39,15 @@ export default function TimeInput({ label, value, onChange }: TimeInputProps) {
     onChange(type, sanitized);
   };
 
+  useFocusOnClick([label + "-h", label + "-m"]);
+
   return (
     <div className="flex gap-4 items-center">
       <Label>{label}</Label>
       <Input
-        type="text"
-        inputMode="numeric"
-        pattern="\d{1,2}"
+        id={label + "-h"}
+        type="number"
+        // pattern="\d{1,2}"
         maxLength={2}
         value={displayValue(value.h, "h")}
         onChange={handleChange("h")}
@@ -39,9 +56,9 @@ export default function TimeInput({ label, value, onChange }: TimeInputProps) {
       />
       <span>:</span>
       <Input
-        type="text"
-        inputMode="numeric"
-        pattern="\d{1,2}"
+        id={label + "-m"}
+        type="number"
+        // pattern="\d{1,2}"
         maxLength={2}
         value={displayValue(value.m, "m")}
         onChange={handleChange("m")}
