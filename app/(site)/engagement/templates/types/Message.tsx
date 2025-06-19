625cb78da3dec92abb3baacd94698d28dd9d8f40
Alessandro Cecchini <alego.cecchini@gmail.com>
2025-06-19 22:51:48 +0200

    Replace Input with Textarea in Message component for better user experience.

--- app/(site)/engagement/templates/types/Message.tsx
+++ app/(site)/engagement/templates/types/Message.tsx
@@ -1,5 +1,6 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 interface MessageProps {
   onChange: (value: string) => void;
@@ -11,7 +12,7 @@ export default function Message({ onChange, value, disabled }: MessageProps) {
   return (
     <div className="w-full flex flex-col space-y-2">
       <Label htmlFor="msg">Messaggio</Label>
-      <Input
+      <Textarea
         disabled={disabled}
         id="msg"
         className="w-full"
