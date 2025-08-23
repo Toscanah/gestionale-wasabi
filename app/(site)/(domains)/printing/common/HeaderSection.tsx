15ff5a647c6139688f41e48db3ad9c21eb16e790
Alessandro Cecchini <alego.cecchini@gmail.com>
2025-08-23 14:35:43 +0200

    Replace fitReceiptText with truncate for better text handling in HeaderSection.

--- app/(site)/(domains)/printing/common/HeaderSection.tsx
+++ app/(site)/(domains)/printing/common/HeaderSection.tsx
@@ -4,7 +4,6 @@ import { GlobalSettings } from "../../../lib/shared/types/Settings";
 import { DEFAULT_SETTINGS } from "../../../hooks/useSettings";
 import sanitazeReceiptText from "../../../lib/utils/domains/printing/sanitazeReceiptText";
 import { BIG_PRINT, SMALL_PRINT } from "../constants";
-import fitReceiptText from "@/app/(site)/lib/utils/domains/printing/fitReceiptText";
 
 // import logo from "../../../../public/logo.png";
 
@@ -17,46 +16,42 @@ export default function HeaderSection({ orderDate }: HeaderSectionProps) {
     localStorage.getItem("settings") || JSON.stringify(DEFAULT_SETTINGS)
   ) as GlobalSettings;
 
+  const truncate = (text: string, maxChar: number) => text.slice(0, maxChar);
+
   return (
     <>
       {/* <Image src={logo.src} width={200} height={200} align="center" /> */}
 
       <Text size={BIG_PRINT} bold align="center">
-        {fitReceiptText(sanitazeReceiptText(settings.name), 20)}
+        {truncate(sanitazeReceiptText(settings.name), 20)}
       </Text>
 
       {settings.slogan && (
         <Text size={SMALL_PRINT} align="center">
-          {fitReceiptText(sanitazeReceiptText(settings.slogan), 40)}
+          {truncate(sanitazeReceiptText(settings.slogan), 40)}
         </Text>
       )}
 
       <Br />
 
       <Text size={SMALL_PRINT} align="center">
-        {fitReceiptText(
-          sanitazeReceiptText(settings.address.street + " " + settings.address.civic),
-          40
-        )}
+        {truncate(sanitazeReceiptText(settings.address.street + " " + settings.address.civic), 40)}
       </Text>
 
       <Text size={SMALL_PRINT} align="center">
-        {fitReceiptText(
-          sanitazeReceiptText(settings.address.cap + " " + settings.address.city),
-          40
-        )}
+        {truncate(sanitazeReceiptText(settings.address.cap + " " + settings.address.city), 40)}
       </Text>
 
       <Text size={SMALL_PRINT} align="center">
-        {fitReceiptText(sanitazeReceiptText(`Tel: ${settings.telNumber}`), 40)}
+        {truncate(sanitazeReceiptText(`Tel: ${settings.telNumber}`), 40)}
       </Text>
 
       <Text size={SMALL_PRINT} align="center">
-        {fitReceiptText(sanitazeReceiptText(`Cell: ${settings.cellNumber}`), 40)}
+        {truncate(sanitazeReceiptText(`Cell: ${settings.cellNumber}`), 40)}
       </Text>
 
       <Text size={SMALL_PRINT} align="center">
-        {fitReceiptText(sanitazeReceiptText(`P.iva ${settings.pIva}`), 40)}
+        {truncate(sanitazeReceiptText(`P.iva ${settings.pIva}`), 40)}
       </Text>
 
       <Br />
